58090f72b8b7be2ac6cf5308a21b0c0115815c9e
Wojciech <woytass@gmail.com>
2015-08-20 13:12:03 +0200

    Exercise 2 - solution

--- Lesson 6 - E2E tests/config/config.js
+++ Lesson 6 - E2E tests/config/config.js
@@ -1,11 +1,23 @@
 exports.config = {
   baseUrl: 'http://localhost:8080',
 
+  chromeOnly: true,
   seleniumServerJar: '../selenium/selenium-server-standalone-2.45.0.jar',
   chromeDriver: '../selenium/chromedriver.exe',
+
+  jasmineNodeOpts: {
+    // If true, display spec names.
+    isVerbose: true,
+    // If true, print colors to the terminal.
+    showColors: true,
+    // If true, include stack traces in failures.
+    includeStackTrace: true,
+    // Default time to wait in ms before a test fails.
+    defaultTimeoutInterval: 30000
+  },
 
   suites: {
     myfirst: '../tests/firstTest.e2e.js',
     //pop: '../tests/pageObjectsTests.e2e.js'
-  },
+  }
 }
--- Lesson 6 - E2E tests/tests/firstTest.e2e.js
+++ Lesson 6 - E2E tests/tests/firstTest.e2e.js
@@ -1,5 +1,53 @@
 describe ('My first test suite', function(){
-  it('Will do nothing', function(){
+  beforeEach(function()
+  {
+    browser.get('');
+    browser.sleep(2000);
+  });
+
+  it('exc1', function(){
+      var toDo = "SthToDo";
+      var inputBox = element(by.model('inputValue'));
+      var submitButton = element(by.css('button[ng-click="add()"]'));
+      var list = element(by.id('tasks')).all(by.repeater('element in elements'));
+      inputBox.sendKeys(toDo);
+      submitButton.click();
+      //first time iterate trough the list
+      list
+      .map(function (item) {
+        return {
+          name: item.getText(),
+          locator: item
+        };
+      })
+      .then(function (collection) {
+        //check for all elements count -> compare to label
+        var allItemsLabel = element(by.css('header .count-all')).getText();
+        expect(allItemsLabel).toEqual(collection.length.toString());
+        //click proper element and check if is highlighted
+        collection.forEach(function (item){
+          if(item.name === toDo)
+          {
+            item.locator.click();
+            expect(item.locator.getAttribute('class')).toContain('done');
+          }
+        });
+      });
+      //check for all to do elements count -> compare to label
+      list
+      .map(function (item)
+      {
+        return item.getAttribute('class');
+      }).
+      then(function(collection){
+        var toDoCnt = 0;
+        var toDoItemsLabel = element(by.css('header .count-todo')).getText();
+        collection.forEach(function(x){
+          if(x.indexOf("done") < 0){toDoCnt++;}
+        });
+        expect(toDoItemsLabel).toEqual(toDoCnt.toString());
+      })
+
 
   });
 });
